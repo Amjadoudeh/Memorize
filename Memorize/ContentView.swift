fe3c5840c78e6c396fd357e9f9ae48e6fd87fa01
Amjadoudeh <Amjad.oudeh88@gmail.com>
2021-10-09 13:22:01 +0200

    using the onTapGesture

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -1,38 +1,17 @@
-//
 //  ContentView.swift
 //  Memorize
 //
 //  Created by Amjad Oudeh on 06.10.21.
-//
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-// Zstack is a bag where I can put my legoes in -> also it making the desigen stick togather :)
-        HStack {
-             ZStack {
-// RoundedRecatngle is a shape and his label is conerRadius -> the value of this lable used to give the border radius for the rectangle
-              RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
-// stroke is a funcation used to give border
-                .stroke(lineWidth: 3.0)
-              Text("Welcome to Rome!!")
-                   }
-             ZStack {
-                RoundedRectangle(cornerRadius: 25.0)
-                .stroke(lineWidth: 3.0)
-              Text("Welcome to Rome!!")
-                    }
-            ZStack {
-               RoundedRectangle(cornerRadius: 25.0)
-               .stroke(lineWidth: 3.0)
-             Text("Welcome to Rome!!")
-                   }
-            ZStack {
-               RoundedRectangle(cornerRadius: 25.0)
-               .stroke(lineWidth: 3.0)
-             Text("Welcome to Rome!!")
-                   }
+        HStack{
+            CardView()
+            CardView()
+            CardView()
+            CardView()
         }
         .padding(.horizontal)
         .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
@@ -41,8 +20,25 @@ struct ContentView: View {
 
 
 // cards
-
-
+struct CardView: View {
+   @State var isFaceUp : Bool = true
+    
+    var body : some View {
+        let shape = RoundedRectangle(cornerRadius: 25.0)
+        ZStack {
+        if isFaceUp {
+            shape.fill().foregroundColor(.white)
+            shape.stroke(lineWidth: 3.0)
+            Text("✈️").font(.largeTitle)
+        } else {
+            shape.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
 
 
 
@@ -75,6 +71,10 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+        ContentView()
+            .preferredColorScheme(.light)
+            
     }
 }
 
