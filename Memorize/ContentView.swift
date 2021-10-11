0dc9ecda1dce068aa1134dfc24f8399098f00304
Amjadoudeh <Amjad.oudeh88@gmail.com>
2021-10-11 15:26:11 +0200

    adapting the Grid to fit the cards also in landscape view

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     var body: some View {
         VStack{
             ScrollView{
-            LazyVGrid(columns: [GridItem(),GridItem(),GridItem()])
+                LazyVGrid(columns: [GridItem(.adaptive(minimum:65))])
             {
                 ForEach(emojis[0..<emojiCount], id: \.self) {
                     emoji in CardView (content: emoji).aspectRatio(2/3, contentMode:.fit)
