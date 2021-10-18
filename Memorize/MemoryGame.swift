743eff1950dc345d338f88fdc4dabdc6330f2f04
Amjadoudeh <Amjad.oudeh88@gmail.com>
2021-10-18 11:27:46 +0200

    new file created " MemoryGame"

---
+++ Memorize/MemoryGame.swift
@@ -0,0 +1,16 @@
+//
+//  MemoryGame.swift
+//  Memorize
+//
+//  Created by Amjad Oudeh on 18.10.21.
+//
+
+import Foundation
+
+struct MemoryGame {
+    var cards: Array<Card>
+    
+    func choose (card: card){
+        
+    }
+}
