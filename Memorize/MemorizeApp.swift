fa79223dbb5964e6239e9d1848ba24e11325534b
Amjadoudeh <Amjad.oudeh88@gmail.com>
2021-11-02 12:57:12 +0100

    made the let game as private in Memorizeapp file, because we have only one app and we don't have to give any access.

--- Memorize/MemorizeApp.swift
+++ Memorize/MemorizeApp.swift
@@ -9,7 +9,7 @@ import SwiftUI
 // the main App 
 @main
 struct MemorizeApp: App {
-    let game = EmojiMemoryGame()
+   private let game = EmojiMemoryGame()
     
     var body: some Scene {
         WindowGroup {
