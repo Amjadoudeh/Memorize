2254adaf856f75b78612e9bd188ae0cbe917b047
Amjadoudeh <Amjad.oudeh88@gmail.com>
2021-11-12 11:34:41 +0100

    added a new File " Pie " and creating a struct Pie behaving like a shape !!

---
+++ Memorize/Pie.swift
@@ -0,0 +1,16 @@
+//
+//  Pie.swift
+//  Memorize
+//
+//  Created by Amjad Oudeh on 12.11.21.
+//
+
+import SwiftUI
+
+struct Pie: Shape {
+    func path(in rect: CGRect) -> Path {
+        <#code#>
+    }
+    
+    
+}
