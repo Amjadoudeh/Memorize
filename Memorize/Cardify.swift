6add2c84cf13ee565038631ddbd9dd2b36fe02fd
Amjadoudeh <Amjad.oudeh88@gmail.com>
2021-11-16 12:40:49 +0100

    created a new file to write our own View-modifier and replacing	 all the little view-modifiers that we using now!

---
+++ Memorize/Cardify.swift
@@ -0,0 +1,15 @@
+//
+//  Cardify.swift
+//  Memorize
+//
+//  Created by Amjad Oudeh on 16.11.21.
+//
+
+import SwiftUI
+
+struct Cardify: ViewModifier {
+    
+    func body(content: Content) -> some View {
+        <#code#>
+    }
+}
